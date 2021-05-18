6e91303d46ccf0a9ff7287e5f5745b50b5ec062d
Bilal Khan <bilal.khan@live.com>
2021-05-18 21:29:15 +1000

    feat/appbar links style when active

--- src/App/AppBar.jsx
+++ src/App/AppBar.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import styled, { css } from "styled-components";
+import { AppContext } from "./AppProvider";
 
 const Logo = styled.div`
   font-size: 1.5em;
   font-weight: bold;
   background: #32333a;
-  padding: 0.5em;
   border-radius: 5px;
+  padding: 0.5em;
   color: aqua;
 `;
 
 const Bar = styled.div`
   display: grid;
   margin-bottom: 50px;
+  padding: 0.5em;
   grid-template-columns: 180px auto 100px 100px;
 `;
 
 const ControlButtonElem = styled.div`
   cursor: pointer;
-  padding: 0.5em;
+
+  &:hover {
+    color: #666876;
+  }
   ${(props) =>
     props.active &&
     css`
@@ -30,9 +35,19 @@ const toProperCase = (lower) => {
   return lower.charAt(0).toUpperCase() + lower.substring(1);
 };
 
-const ControlButton = ({ name, active }) => {
+const ControlButton = ({ name }) => {
   return (
-    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+    <AppContext.Consumer>
+      {({ page, setPage }) => (
+        <ControlButtonElem
+          active={page === name}
+          onClick={() => setPage(name)}
+          hover={page === name}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+      )}
+    </AppContext.Consumer>
   );
 };
 
@@ -41,7 +56,7 @@ const AppBar = () => {
     <Bar>
       <Logo>Cryptodash</Logo>
       <div />
-      <ControlButton name="dasboard" active />
+      <ControlButton name="dasboard" />
       <ControlButton name="settings" />
     </Bar>
   );
--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,19 @@
 import './App.css';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar'
+import AppProvider from './AppProvider';
 function App() {
-  return (
-    <div className="App">
-		<AppLayout>
-			<AppBar/>
-		  <h1>Welcome to Cryptodash</h1>
-		</AppLayout>
+	return (
+		<div className="App">
+			<AppLayout>
+				<AppProvider>
+					<AppBar />
+					<h1>Welcome to Cryptodash</h1>
+				</AppProvider>
+			</AppLayout>
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
