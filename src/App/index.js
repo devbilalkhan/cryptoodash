d846b0f0108ff1e7b8185425f30d7ad64cd5d5f3
Bilal Khan <bilal.khan@live.com>
2021-05-18 20:17:55 +1000

    feat/appbar

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AppLayout from './AppLayout'
+import AppBar from './AppBar'
 function App() {
   return (
     <div className="App">
 		<AppLayout>
+			<AppBar/>
 		  <h1>Welcome to Cryptodash</h1>
 		</AppLayout>
 
